ae8d421293ac97ff40c4bddc72f79d8b0ba20dc2
Carlos Cima <carlos.cima@linio.com>
2017-11-01 22:53:35 -0400

    Fixed issues when running tests with line breaks on windows.

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -11,6 +11,6 @@ abstract class AbstractTest extends \PHPUnit_Framework_TestCase
      */
     protected function getDatabaseFixture($filename)
     {
-        return file_get_contents(__DIR__ . '/fixtures/' . $filename);
+        return preg_replace("/\r?\n/", PHP_EOL, file_get_contents(__DIR__ . '/fixtures/' . $filename));
     }
 }
