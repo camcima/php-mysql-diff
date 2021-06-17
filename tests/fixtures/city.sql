d0390d812253af5cff2694a035c091556d4f2ad7
Lars Plessmann <l.plessmann@powercloud.de>
2021-06-17 14:29:34 +0200

    Added comment to testing table city.sql

--- tests/fixtures/city.sql
+++ tests/fixtures/city.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `city` (
   `city_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `zip` int(5) unsigned zerofill NOT NULL,
-  `lat` decimal(8,6) unsigned NOT NULL,
-  `lng` decimal(9,6) unsigned NOT NULL,
+  `lat` decimal(8,6) unsigned NOT NULL DEFAULT '0.000000' COMMENT 'latitude',
+  `lng` decimal(9,6) unsigned NOT NULL DEFAULT '0.000000' COMMENT 'longitude',
   `city_name` varchar(90) NOT NULL,
   `country_code` varchar(2) NOT NULL,
   `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
